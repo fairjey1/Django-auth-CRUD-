87311d05eb32953fc872c63bfe97857c2c7c33d6
Justo Arouxet <fairjey1@gmail.com>
2026-02-07 13:12:57 -0300

    change settings

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -122,16 +122,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not DEBUG:
+'''if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' '''
+DEBUG = True
 
 LOGIN_URL = '/signin'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://django-auth-crud-s5s4.onrender.com',
+    'https://django-auth-crud-s5s4.onrender.com'
 ]
